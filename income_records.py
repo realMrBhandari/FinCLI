37b25e7b51854f0de94b78174d63b414e269f786
realMrBhandari <23021086@geu.ac.in>
2026-01-27 19:13:33 +0530

    Refactored income category, implemented predefined income categories

--- income_records.py
+++ income_records.py
@@ -20,17 +20,36 @@ print(
 category = input("Please input your income source: \t")
 income_source = None
 
-match category:
-    case "1":
-        income_source = "Business"
-    case "2":
-        income_source = "Freelancing"
-    case "3":
-        income_source = "Salary"
-    case "4":
-        income_source = "Pocket Money"
-    case "5":
-        income_source = "Other Sources"
+# match category:
+#     case "1":
+#         income_source = "Business"
+#     case "2":
+#         income_source = "Freelancing"
+#     case "3":
+#         income_source = "Salary"
+#     case "4":
+#         income_source = "Pocket Money"
+#     case "5":
+#         income_source = input("Please specify your source")
+#     case _:
+#          while
+
+if category == "1":
+    income_source = "Business"
+elif category == "2":
+    income_source = "Freelancing"
+elif category == "3":
+    income_source = "Salary"
+elif category == "4":
+    income_source = "Pocket Money"
+elif category == "5":
+    income_source = input("Please specify your source")
+else:
+    while category != ["1", "2", "3", "4", "5"]:
+        print("Your input was invalid:")
+        category = input("Please re-enter your income source: \t")
+
+
 print(income_source, income_amount)
 
 # todo:  Implement category vlaidation
