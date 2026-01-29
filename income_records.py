bfe6b3408f0be2e20f9e2c8d31a47d6b465fff48
realMrBhandari <23021086@geu.ac.in>
2026-01-29 22:21:14 +0530

    Income Category Validation Implemented

--- income_records.py
+++ income_records.py
@@ -1,55 +1,33 @@
+# ! Input Amount Code
 print("=== Your Income Records ===")
-
 income_amount = input(
     "Please input your income amount (Should be positive and numeric): \t"
 )
-
-# ? .isdigit(), isnumber() are only for strings, can be used to validate if user has entered numeric values in case of input() | i guess there's some difference between these methods, need to dig into it.
-
 if not income_amount.isdigit():
     while not income_amount.isdigit():
         print("Attention User! Your input was non numeric ￣へ￣")
         income_amount = input("Re-enter your income, make sure input is numeric: \t")
-
 # todo: Implement validation for negative & 0 inputs as well
 
-
+# ! Income Source Section
+income_source = None
 print(
     "\n=== Please Enter Your Income Source ===\n1. Business \n2. Freelancing\n3. Salary \n4. Pocket Money \n5. Others "
 )
 category = input("Please input your income source: \t")
-income_source = None
-
-# match category:
-#     case "1":
-#         income_source = "Business"
-#     case "2":
-#         income_source = "Freelancing"
-#     case "3":
-#         income_source = "Salary"
-#     case "4":
-#         income_source = "Pocket Money"
-#     case "5":
-#         income_source = input("Please specify your source")
-#     case _:
-#          while
-
-if category == "1":
-    income_source = "Business"
-elif category == "2":
-    income_source = "Freelancing"
-elif category == "3":
-    income_source = "Salary"
-elif category == "4":
-    income_source = "Pocket Money"
-elif category == "5":
-    income_source = input("Please specify your source")
-else:
-    while category != ["1", "2", "3", "4", "5"]:
-        print("Your input was invalid:")
-        category = input("Please re-enter your income source: \t")
-
-
-print(income_source, income_amount)
-
-# todo:  Implement category vlaidation
+valid_inputs = ["1", "2", "3", "4", "5"]
+while category not in valid_inputs:
+    print("Your input was invalid:")
+    category = input("Please re-enter your income source: \t")
+match category:
+    case "1":
+        income_source = "Business"
+    case "2":
+        income_source = "Freelancing"
+    case "3":
+        income_source = "Salary"
+    case "4":
+        income_source = "Pocket Money"
+    case "5":
+        income_source = input("Please specify your source:\t")
+        # TODO ONE MORE CONDITION SHOULD BE IMPLEMENTED HERE WHERE THERE SHOULD BE A DEFAULT CASE IN CASE USER DOES NOT PROVIDE ANY INCOME SOURCE
