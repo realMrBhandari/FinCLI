5c02c901d43fde1851d7c9b0c944784df863fe46
realMrBhandari <23021086@geu.ac.in>
2026-02-09 17:32:31 +0530

    Reworked on FinCLI Finance Menu

--- fincli_menu.py
+++ fincli_menu.py
@@ -5,8 +5,18 @@ print(
                              FinCLI Finance Mneu
 ===========================================================================\033[0m\n 1. Income Records\n 2. Expense Records\n 3. View all transactions \n 4. Monthly Spends Summary \n 5. Exit \n"""
 )
+navigate = input("Pick a choice:\t")
 
-navigate = input("Pick a choice")
-
+choices = ["1", "2", "3", "4", "5"]
+while navigate not in choices:
+    navigate = input("Your choice was invalid! Try Again:\t")
 if navigate == "1":
     income_records.income_recorder()
+elif navigate == "2":
+    print("\vFunctionality Under Development.")
+elif navigate == "3":
+    print("\vFunctionality Under Development.")
+elif navigate == "4":
+    print("\vFunctionality Under Development.")
+elif navigate == "5":
+    print("\vFunctionality Under Development.")
