eb86a54979f8a37263e40a14e24c7440e532ce49
realMrBhandari <23021086@geu.ac.in>
2026-02-12 12:23:17 +0530

    input to int conversion, todos added

--- income_records.py
+++ income_records.py
@@ -6,18 +6,20 @@ def income_recorder():
                              YOUR INCOME RECORDS
 ===========================================================================\033[0m\v"""
     )
+    # ? Income recording section
+    # !================     REFACTORING REGION  ================================================
     print("*income amount should be positive and numeric")
-    income_amount = input("Please input your income amount : \t")
-    while not income_amount.isdigit():
+    input_income = input("Please input your income amount : \t")
+    # todo: Implement trype conversion to float here and make sure the it is in two decimal points i.e in indian decimal system for recording monitoary value, also implement exception handling here so in case user passes some non numeric input here then the program should not crash while input get's converted into float and rather ask user again for valid numeric input and once float conversion is successfull the programme should moveon. So far this is the biggest obstacle for me in this code, I will proccede with integer input as of now and later convert it to floating type
+
+    while not input_income.isdigit():
         print("\033[31mAttention User! Your input was invalid.\033[0m")
         income_amount = input("Only positive and numeric input allowed. Try again: \t")
-    # todo: A type conversion is needed here, the user input which is string is needed to be converted into a numeric type once input for income amount is validated and implement expection handeling.
 
-    # todo: Implement validation for negative & 0 inputs as well
+    income_amount = int(input_income)
+    # !================     REFACTORING REGION  ================================================
 
-    # todo: implement the industrial level of recording money, i guess money is not recorded in floating points, but whatever system it is money will be stored in that method.
-
-    # ! Income Source Section
+    # ? Income Source Section
     income_source = None
     print(
         "\n=== Please Enter Your Income Source ===\n1. Business \n2. Freelancing\n3. Salary \n4. Pocket Money \n5. Others "
@@ -38,9 +40,7 @@ def income_recorder():
         case "5":
             income_source = (
                 input("Please specify your source:\t") or "Other sources"
-            )  # or <value> allows us to define a default value with input statement in case user submits blank value.
-        # TODO ONE MORE CONDITION SHOULD BE IMPLEMENTED HERE WHERE THERE SHOULD BE A DEFAULT CASE IN CASE USER DOES NOT PROVIDE ANY INCOME SOURCE
-
+            )  # ? or <value> allows us to define a default value with input statement in case user submits blank value.
     print(
         f"\vYou earned \033[33m{income_amount} \033[0mfrom \033[33m{income_source}\033[0m, use it wisely."
     )
