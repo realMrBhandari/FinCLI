5b5c802fe96ef2035ef3838b3c7d2706b0bf3ede
realMrBhandari <23021086@geu.ac.in>
2026-03-03 13:46:28 +0530

    Structured income transaction data with notes and account details and a validation layer.

--- income_records.py
+++ income_records.py
@@ -2,23 +2,27 @@ import module_floatChk
 
 
 def income_recorder():
+    #  Global Variable Zone
+    income_transactions = (
+        {}
+    )  # ? dicitonary as a middlemen between userinputs and database
     print(
         """\033[1;32m===========================================================================
                              YOUR INCOME RECORDS
 ===========================================================================\033[0m\v"""
     )
+    # def income_inputs():
     # ? Income recording section
     # !================     REFACTORING REGION  ================================================
     print("*input should be positive and numeric")
     input_income = input("Please input your income amount:\t")
 
     # ? Short circuit is preventing programme from crashing here.
-    while (not module_floatChk.isItFloat(input_income)) or float(input_income) < 0:
+    while (not module_floatChk.isItFloat(input_income)) or float(input_income) <= 1:
         print("\033[31mAttention User! Your input was invalid.\033[0m")
         input_income = input("Only positive and numeric input allowed. Try again: \t")
 
     income_amount = round(float(input_income), 2)
-
     # !================     REFACTORING REGION  ===========================================
     # ? Income Source Section
     income_source = None
@@ -42,13 +46,46 @@ def income_recorder():
                 input("Please specify your source:\t") or "Other sources"
             )  # ? or <value> allows us to define a default value with input statement in case user submits blank value.
     transaction_location = (
-        input("Enter the account in which the income was credited:\t") or "bank"
+        input("\nEnter the account in which the income was credited:\t") or "bank"
     )
+    transaction_note = input(
+        "\nAdd a note to describe your transaction (50 characters max): "
+    )
+    while len(transaction_note) > 50 or len(transaction_note) < 1:
+        print(
+            "\033[31mInvalid Input! Note should be between 1 to 50 characters long, try again!\033[0m"
+        )
+        transaction_note = input("Add a note to describe your transaction: ")
+
+    income_transactions["income_amount"] = income_amount
+    income_transactions["income_source"] = income_source
+    income_transactions["transaction_location"] = transaction_location
+    income_transactions["transaction_note"] = transaction_note
+
     # todo : implement transaction location feture as well, to differentiate where money was deposited to and where it was withdrawn from: transactionLocation, and implement a networth like feature showing where all money resides and for montly spends and transaction summary also add columns to show remaining balanaces in all accounts.
 
     # todo: implement date of transaction recodring as well, with a default and custom date feature.
 
     # ! To implement Recording these information to a database for transaction record and keeping track of balance
+
+    # ! Implement Final Table in which records will be displayed to user
     print(
-        f"\vRecorded income amount of \033[33m{income_amount} \033[0mfrom \033[33m{income_source}\033[0m recieved in \033[33m{transaction_location}\033[0m account."
+        f"\vRecorded income amount of \033[33m{income_transactions['income_amount']} \033[0mfrom \033[33m{income_transactions['income_source']}\033[0m credited in \033[33m{income_transactions['transaction_location']}\033[0m account, was done for \033[33m{income_transactions['transaction_note']}\033[0m."
     )
+
+
+# ! Decision State between user input and DB
+# def input_Write():
+#     print("Do You wish to finalise transaction ?")
+#     user_decision = input(
+#         "Type CONFIRM to finalise, else type REVERT to go back: "
+#     ).lower()
+#     while user_decision not in ["confirm", "revert"]:
+#         print("Invalid Input! Try Again !")
+#         user_decision = input(
+#             "Type CONFIRM to finalise, else type REVERT to go back: "
+#         ).lower()
+#     if user_decision == "revert":
+#         income_inputs()
+#     else:
+#         # add the values stright into json
