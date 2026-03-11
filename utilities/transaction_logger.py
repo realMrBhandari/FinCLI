72feeecbc3ccc7606e4d3fc46db99af7474bec06
realMrBhandari <23021086@geu.ac.in>
2026-03-11 23:39:53 +0530

    feat: implemented tabular summary for recorded income transaction for better UX and programme logic

--- utilities/transaction_logger.py
+++ utilities/transaction_logger.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 import json
 import os
+from tabulate import tabulate
 
 
 def saveTransaction(transaction):
@@ -20,6 +21,30 @@ def saveTransaction(transaction):
     with open("data/transactions.json", "w") as json_file:
         json.dump(stored_transactions, json_file, indent=4)
 
+    # print(
+    #     f"\vRecorded income amount of \033[33m{transaction['income_amount']} \033[0mon \033[33m{transaction['transaction_date']} \033[0mfrom \033[33m{transaction['income_source']}\033[0m credited in \033[33m{transaction['transaction_location']}\033[0m account, was done for \033[33m{transaction['transaction_note']}\033[0m."
+    # )
+    print("""\n\033[32mTransaction recorded successfully.\033[0m""")
+
+    table = [
+        [
+            transaction["income_amount"],
+            transaction["transaction_date"],
+            transaction["income_source"],
+            transaction["transaction_location"],
+            transaction["transaction_note"],
+        ]
+    ]
     print(
-        f"\vRecorded income amount of \033[33m{transaction['income_amount']} \033[0mon \033[33m{transaction['transaction_date']} \033[0mfrom \033[33m{transaction['income_source']}\033[0m credited in \033[33m{transaction['transaction_location']}\033[0m account, was done for \033[33m{transaction['transaction_note']}\033[0m."
+        tabulate(
+            table,
+            headers=[
+                "\033[33mAmount Cr.\033[0m",
+                "\033[33mTransaction Date\033[0m",
+                "\033[33mIncome Source\033[0m",
+                "\033[33mAccount\033[0m",
+                "\033[33mNote\033[0m",
+            ],
+            tablefmt="simple_outline",
+        )
     )
