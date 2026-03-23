4ef6669ad67ca689630c22b1223a5eea95666656
realMrBhandari <23021086@geu.ac.in>
2026-03-24 00:40:27 +0530

    fix: fixed table row addition bug in transaction summary module

--- core/transaction_summary.py
+++ core/transaction_summary.py
@@ -13,11 +13,12 @@ def show_transaction_statement():
     with open("./data/transactions.json") as file:
         transactions = json.load(file)
 
-    # ? for extracting transaction data from nested dictionaries in json
+    # ? for extracting transaction data from nested dictionaries and appending it to table row: ps this caused bug the list not getting appened in as nested dictionaries but rather individual values in transaction_summary_table
     for txn in transactions.values():
         for txn_values in txn.values():
             transaction_table_row.append(txn_values)
         transaction_summary_table.append(transaction_table_row)
+        transaction_table_row = []
 
     # ? for printing transaction summary in a tabular format like account statement
     section_heading = "\033[1;32m---------------------FinCLI Transaction Summary---------------------\033[0m"
