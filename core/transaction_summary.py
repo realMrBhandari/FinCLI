4105155d7297afd7f3e7ac5488096d2318228725
realMrBhandari <23021086@geu.ac.in>
2026-03-30 15:45:52 +0530

    refactor: csv export directory changed to exports/ from root

--- core/transaction_summary.py
+++ core/transaction_summary.py
@@ -3,18 +3,19 @@ import csv
 from tabulate import tabulate
 
 
-# todo 2: Add export to CSV Option in this transaction summary
-# todo 3: Add graphs for transactions for showing flow
+# todo 1: convert export to CSV to a module
+# todo 2: fix CREDIT DEBIT data in CSV with ASCII Escape Sequennce codes coming in it
+# todo 4: Add graphs for transactions for showing flow
 def show_transaction_statement():
-    # ? for variables, first transaction_summary_table for complete table &  transaction_table_row for individual row
-    transaction_summary_table = []
+    # ? for variables, first transaction_logs_data for complete table &  transaction_table_row for individual row
+    transaction_logs_data = []
     transaction_table_row = []
 
     # ? Loading json as a dictionary
     with open("./data/transactions.json") as file:
         transactions = json.load(file)
 
-    # ? for extracting transaction data from nested dictionaries and appending it to table row: ps this caused bug the list not getting appened in as nested dictionaries but rather individual values in transaction_summary_table
+    # ? for extracting transaction data from nested dictionaries and appending it to table row: ps this caused bug the list not getting appened in as nested dictionaries but rather individual values in transaction_logs_data
     for txn in transactions.values():
         for txn_values in txn.values():
             if txn_values == "CREDIT":
@@ -23,20 +24,20 @@ def show_transaction_statement():
                 transaction_table_row.append("\033[31mDEBIT\033[0m")
             else:
                 transaction_table_row.append(txn_values)
-        transaction_summary_table.append(transaction_table_row)
+        transaction_logs_data.append(transaction_table_row)
         transaction_table_row = []
 
-    # ? Sorting 2D List (transaction_summary_table, which contains all data of trasnaction to be displayed) based on date which is at index[0] hence the output comes based on chronological order
-    transaction_summary_table.sort(key=lambda x: x[0])
+    # ? Sorting 2D List (transaction_logs_data, which contains all data of trasnaction to be displayed) based on date which is at index[0] hence the output comes based on chronological order
+    transaction_logs_data.sort(key=lambda x: x[0])
     # ? for printing transaction summary in a tabular format like account statement
-    if len(transaction_summary_table) < 1:
+    if len(transaction_logs_data) < 1:
         print("\033[31mIt looks like you haven't made any transactions so far.\033[0m")
     else:
         section_heading = "\033[1;32m---------------------FinCLI Transaction Summary---------------------\033[0m"
         print(section_heading)
         print(
             tabulate(
-                transaction_summary_table,
+                transaction_logs_data,
                 headers=[
                     "\033[33mDate\033[0m",
                     "\033[33mCategory\033[0m",
@@ -69,9 +70,8 @@ def show_transaction_statement():
             "Bank_Account",
             "Trasnaction_Note",
         ]
-        with open("Fincli_transaction_data.csv", "w", newline="") as csv_file:
+        with open(f"exports/Fincli_transaction_data.csv", "w", newline="") as csv_file:
             writer = csv.writer(csv_file)
             writer.writerow(header_data)
-            writer.writerows(transaction_summary_table)
-
-    print("\n\033[32mTransaction successfully exported!\033[0m\n")
+            writer.writerows(transaction_logs_data)
+        print("\n\033[32mTransaction successfully exported!\033[0m\n")
